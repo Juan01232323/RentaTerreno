f673c0c0c08dd419b13b9adb7e985d3538f901a3
Juan Carlos Reynoso Zuñiga <116603804+Juan01232323@users.noreply.github.com>
2024-10-02 00:32:21 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,5 @@
 
+
 // Función para obtener las reservas del LocalStorage
 function obtenerReservas() {
     const reservas = localStorage.getItem('reservas');
@@ -62,7 +63,7 @@ function mostrarReservas() {
 
 // Función para obtener las reservas desde el servidor (API)
 function obtenerReservasServidor() {
-    fetch('https://f29c-2806-261-a401-55f-7c88-ee9f-9e2c-9538.ngrok-free.app/reservas')
+    fetch('https://347d-2806-261-a401-55f-7c88-ee9f-9e2c-9538.ngrok-free.app/reservas')
         .then(response => response.json())
         .then(data => {
             mostrarReservasServidor(data);
@@ -137,7 +138,7 @@ function mostrarReservasConTiempoRestante() {
 
 // Función para eliminar una reserva
 function eliminarReserva(id) {
-    fetch(`https://f29c-2806-261-a401-55f-7c88-ee9f-9e2c-9538.ngrok-free.app/reservas/${id}`, {
+    fetch(`https://347d-2806-261-a401-55f-7c88-ee9f-9e2c-9538.ngrok-free.app/reservas/${id}`, {
         method: 'DELETE',
     })
     .then(response => {
@@ -204,7 +205,7 @@ document.getElementById('reservationForm').addEventListener('submit', function(e
     }
 
     // Envío de los datos al servidor
-    fetch('https://f29c-2806-261-a401-55f-7c88-ee9f-9e2c-9538.ngrok-free.app/reservas', {
+    fetch('https://347d-2806-261-a401-55f-7c88-ee9f-9e2c-9538.ngrok-free.app/reservas', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -232,7 +233,7 @@ document.getElementById('reservationForm').addEventListener('submit', function(e
 
 function obtenerReservasConPolling() {
     setInterval(() => {
-        fetch('https://f29c-2806-261-a401-55f-7c88-ee9f-9e2c-9538.ngrok-free.app/reservas')
+        fetch('https://347d-2806-261-a401-55f-7c88-ee9f-9e2c-9538.ngrok-free.app/reservas')
             .then(response => response.json())
             .then(data => {
                 mostrarReservasServidor(data); // Actualizar la lista de reservas en el DOM
