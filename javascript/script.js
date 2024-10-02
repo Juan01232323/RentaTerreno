efb52cdd8b5466ecb1a672d73df5f85cf0e74fe2
Juan Carlos Reynoso Zuñiga <116603804+Juan01232323@users.noreply.github.com>
2024-10-01 23:30:51 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -63,7 +63,7 @@ function mostrarReservas() {
 
 // Función para obtener las reservas desde el servidor (API)
 function obtenerReservasServidor() {
-    fetch('http://localhost:3000/reservas')
+    fetch('roughly-picked-humpback.ngrok-free.app/reservas')
         .then(response => response.json())
         .then(data => {
             mostrarReservasServidor(data);
@@ -138,7 +138,7 @@ function mostrarReservasConTiempoRestante() {
 
 // Función para eliminar una reserva
 function eliminarReserva(id) {
-    fetch(`http://localhost:3000/reservas/${id}`, {
+    fetch(`roughly-picked-humpback.ngrok-free.app/reservas/${id}`, {
         method: 'DELETE',
     })
     .then(response => {
@@ -205,7 +205,7 @@ document.getElementById('reservationForm').addEventListener('submit', function(e
     }
 
     // Envío de los datos al servidor
-    fetch('http://localhost:3000/reservas', {
+    fetch('roughly-picked-humpback.ngrok-free.app/reservas', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -233,7 +233,7 @@ document.getElementById('reservationForm').addEventListener('submit', function(e
 
 function obtenerReservasConPolling() {
     setInterval(() => {
-        fetch('http://localhost:3000/reservas')
+        fetch('roughly-picked-humpback.ngrok-free.app/reservas')
             .then(response => response.json())
             .then(data => {
                 mostrarReservasServidor(data); // Actualizar la lista de reservas en el DOM
