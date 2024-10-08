ddac82ea0494a8f1076d81389ffdf7352486a564
Juan Carlos Reynoso Zuñiga <116603804+Juan01232323@users.noreply.github.com>
2024-10-08 15:09:44 -0600

    Update pag.js

--- javascript/pag.js
+++ javascript/pag.js
@@ -11,10 +11,40 @@ function showSlides(n) {
     });
 }
 
+// Prevenir la descarga de imágenes de la galería y mostrar modal
+document.querySelectorAll('.gallery-item img').forEach(img => {
+    img.addEventListener('contextmenu', function(event) {
+        event.preventDefault(); // Prevenir el menú contextual
+        showModal(); // Mostrar modal
+    });
+});
+
+// Función para mostrar el modal
+function showModal() {
+    const modal = document.getElementById("customModal");
+    const closeBtn = document.querySelector(".close");
+
+    modal.style.display = "block"; // Mostrar modal
+
+    // Cerrar modal cuando se haga clic en la "X"
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    // Cerrar modal si se hace clic fuera del contenido del modal
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
+}
+
 document.querySelector('.prev').addEventListener('click', () => {
     slideIndex--;
     showSlides(slideIndex);
 });
+
+
 
 document.querySelector('.next').addEventListener('click', () => {
     slideIndex++;
